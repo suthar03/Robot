78ecc1956b42618c34c688a7f719565814b3dc64
Kanti Lal Suthar <suthar.3@iitj.ac.in>
2021-11-30 18:24:33 +0530

    Robot End Pointer Coordinates

---
+++ src/robot/Robot.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robot;
+
+/**
+ *
+ * @author suthar
+ */
+public class Robot {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Robot_Main_Menu().setVisible(true);
+    }
+    
+}
